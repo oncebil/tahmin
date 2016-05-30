469d09eed352404b68358c338c4c4de41e8f6fe1
oncebil <erkinkarincaoglu@erkins-MacBook-Pro.local>
2016-05-30 21:47:39 +0100

    sql history

---
+++ src/main/resources/sql_history.sql
@@ -0,0 +1,483 @@
+select sum(genyen) from ( a   select ganyan as genyen, sonucNo,gm_RegressionPredicted,ganyan, * from gm_KosuTahminNominal  , AtKosu
+where AtKosu.KosuKodu = gm_KosuTahminNominal.gm_KosuId
+and AtKosu.AtKodu = gm_KosuTahminNominal.gm_AtId
+and gm_RegressionPredicted <0.4
+--AND gm_regressionActual = 1
+and gm_experiment = 'Son7Kosu-SMO-0'
+--and  gm_KosuId = 47978
+order by gm_KosuId,gm_RegressionPredicted
+) A
+
+
+select sum(genyen) from (
+   select ganyan as genyen, sonucNo,gm_RegressionPredicted,ganyan, * from gm_KosuTahminNominal  , AtKosu
+where AtKosu.KosuKodu = gm_KosuTahminNominal.gm_KosuId
+and AtKosu.AtKodu = gm_KosuTahminNominal.gm_AtId
+--and gm_yesprediction > 0.86
+--AND sonucno = 1
+and gm_experiment = 'Son7Kosu-SMO-0'
+and  gm_KosuId = 48027
+order by gm_KosuId,gm_yesprediction
+) A
+
+
+
+
+3.3  14.25   40
+3.1   5.95   12
+
+35  65
+
+2.6 63
+
+
+
+48
+
+
+ 9.55
+
+
+
+17.3  27
+15.95
+
+
+
+SELECT
+ column_name
+FROM
+ information_schema.COLUMNS
+WHERE
+ TABLE_NAME = 'gm_kosutahminnominal';
+
+
+
+select *  from gm_KosuTahminNominal  where gm_experiment = 'Son7Kosu-SMO-0' ORDER BY GM_KOSUID, GM_REGRESSIONPREDICTED;
+
+select *  from gm_KosuTahminNominal ;
+
+select * from AtKosu;
+
+
+select count(*) from Atkosu;
+
+select * from Atkosu where kosukodu = 80047953 and Atkodu = 80022519;
+
+
+SELECT * FROM AtKosu WHERE KosuKodu in (47947,48162,48163,47953,48020,48030) order by KosuKodu,AtKodu
+
+commit;
+
+select distinct gm_KOsuId from gm_KosuTahminNominal  where gm_experiment = 'Son7Kosu-SMO-0' and gm_prediction = 'Y' order by gm_KosuId;
+
+delete from gm_KosuTahminNominal;
+
+select distinct gm_KOsuId from gm_KosuTahminNominal  where gm_experiment = 'Son7Kosu-SMO-0' and gm_prediction = 'N'  order by gm_KosuId;
+
+153 kosu var
+
+
+
+-F "weka.filters.unsupervised.attribute.RemoveType -T string"  -W weka.classifiers.lazy.KStar  -- -B 20 -M a
+
+3.0  63    72
+2.9  54    58
+2.8  47.9   49
+2.7  29.5  38
+2.6  29.5   34
+2.5   28.1   29
+2.4  25.9      23   ------- kazanirdik
+2.3   23.1    21 ------- kazanirdik
+2.2  20.25   16 -- kazanirdik 1.26
+2.1  18.15  14 -- kazanirdik
+
+-F "weka.filters.unsupervised.attribute.RemoveType -T string"  -W weka.classifiers.lazy.KStar --  -B 12 -M a
+
+2.4 78.5 78 -- kazanirdik
+2.3  71.1  70 -- kazanirdik
+2.2  64.9 55 -- kazanirdik  1.18
+2.1 50.2  46 --kazanirdik
+2.0  32.55 36 kaybederdik
+
+
+
+2.4  18.45 13 -- kazanirdik
+2.5   20.65     19 -- kazanirdik
+2.6  22.5 25
+
+
+3.2  14 37
+3.1  10   25
+4.3   246    519
+3.8  201  353
+3.5  132  221
+3.0   19.75  61
+2.9 13.3  42
+2.7   2.45  25
+3,1 32.5 81
+
+49
+47.9
+
+297
+54.7
+
+
+
+
+
+
+
+49
+47
+
+
+38
+29
+
+
+42
+
+
+
+
+
+
+65 aldik
+
+
+
+90 aldik
+
+
+29 verdik
+
+19 aldik
+
+
+
+
+
+47978
+
+182 verdik
+
+122 kazandik
+
+93 verdik
+48 kazandik
+
+-- 157 kosu var
+
+
+   select sonucNo,gm_RegressionPredicted,ganyan, * from gm_KosuTahminNominal  , AtKosu
+where AtKosu.KosuKodu = gm_KosuTahminNominal.gm_KosuId
+and AtKosu.AtKodu = gm_KosuTahminNominal.gm_AtId
+and AtKosu.KosuKodu in (    select gm_KosuId from gm_KosuTahminNominal where gm_RegressionActual = 0 and gm_RegressionPredicted <100 )
+and gm_experiment = 'Son7Kosu-SMO-0'
+order by gm_KosuId, gm_RegressionPredicted
+
+select * from gm_KosuTahminNominal where gm_experiment = 'Son7Kosu-SMO-0'
+
+order by gm_KosuId,gm_RegressionPredicted;
+
+
+
+
+select sum(genyen) from (
+select C.SonucNo, C.ganyan as genyen, A.*,C.* from gm_KosuTahminNominal A,
+(select gm_KosuId,min(gm_RegressionPredicted) as minderece from gm_KosuTahminNominal where gm_experiment = 'Son7Kosu-SMO-0'
+group by gm_KosuId
+order by gm_KosuId ) B,
+AtKosu C
+where A.gm_KosuId = B.gm_KosuId
+and A.gm_RegressionPredicted = B.minderece
+--and B.minderece <100
+and C.AtKodu = A.gm_AtId
+and C.KosuKodu = A.gm_KosuId
+--and A.gm_RegressionActual = 0
+and C.Son7BitirisOrtalamasi < 3.60
+order by A.gm_KosuId ) D
+
+--201 kosuvar
+ -- 57
+
+
+-- 28  kosuda bilirdik
+
+
+select * from gm_KosuTahminNominal where gm_KosuId = 48440 order by gm_RegressionPredicted;
+
+
+
+select sonucNo,gm_RegressionPredicted, * from gm_KosuTahminNominal  , AtKosu
+where AtKosu.KosuKodu = gm_KosuTahminNominal.gm_KosuId
+and  AtKosu.KosuKodu = 47956
+and AtKosu.AtKodu = gm_KosuTahminNominal.gm_AtId
+   order by gm_KosuId, gm_RegressionPredicted
+
+
+
+   select distinct gm_experiment from gm_KosuTahminNominal
+
+   select  *  from gm_KosuTahminNominal where gm_experiment = 'Son7Kosu-SMO-0' and gm_kosuId = 0;
+
+
+   select count(*) as count from (select gm_KosuId, gm_AtId, gm_Prediction,gm_YesPrediction,gm_NoPrediction ,  gm_RegressionActual, gm_RegressionPredicted, gm_RegressionError,
+b.SONUCNO  as Kacinci, b.Ganyan as Ganyan,
+Ikili, SiraliIkili
+from gm_KosuTahminNominal a,
+AtKosu b,
+(select ATKOSU.KOSUKODU as KOSUKODU, ATKODU, Tutar as Ikili
+from ATKOSU
+LEFT OUTER JOIN Bahisler
+ON  ATKOSU.KOSUKODU = Bahisler.KOSUKODU
+and Bahisler.BahisTipAdi='İKİLİ' ) as c,
+(select ATKOSU.KOSUKODU as KOSUKODU, ATKODU, Tutar as SiraliIkili
+from ATKOSU
+LEFT OUTER JOIN Bahisler
+ON  ATKOSU.KOSUKODU = Bahisler.KOSUKODU
+and Bahisler.BahisTipAdi='SIRALI İKİLİ' ) as  d
+ where a.gm_KosuId = b.KOSUKODU
+ and a.gm_AtId = b.ATKODU
+ and a.gm_KosuId = c.KOSUKODU
+ and a.gm_AtId = c.ATKODU
+ and a.gm_KosuId = d.KOSUKODU
+ and a.gm_AtId = d.ATKODU
+ and a.gm_Experiment = 'test-predictions'
+ order by gm_kosuId asc, b.SONUCNO asc  ) as Analiz
+
+
+
+   selec
+
+   select count (*) from (
+   select sonucNo,gm_RegressionPredicted,ganyan, * from gm_KosuTahminNominal  , AtKosu
+where AtKosu.KosuKodu = gm_KosuTahminNominal.gm_KosuId
+
+and AtKosu.AtKodu = gm_KosuTahminNominal.gm_AtId
+and AtKosu.KosuKodu in (    select gm_KosuId from gm_KosuTahminNominal where gm_RegressionActual = 0 and gm_RegressionPredicted <100 )
+order by gm_KosuId, gm_RegressionPredicted
+) A
+group by gm_KosuId
+
+
+   select * from AtKosu where KosuKodu in (
+    select B.kosukodu from
+    (select kosukodu,count(*) as atcount from AtKosu group by kosukodu) A,
+    (select kosukodu,count(*) as atcount from AtKosu where son7derecefarkiortalamasi is not null group by kosukodu ) B
+    where A.kosukodu = B.kosukodu
+    AND A.atcount = B.atcount)
+    AND KOSUTARIHI<='2011-08-01'
+    AND KOSUTARIHI>='2011-05-01'
+    AND BirinciIleDereceFarki is not null    AND substring(KosuCinsDetayAdi from 1 for 8) <> 'Handikap'     order by KosuKodu asc,SonucNo asc
+
+
+
+   -- 2011-05-01
+
+   select sonucNo , * from AtKosu where Atkodu = 21354 order by KosuTarihi desc;
+
+      select sonucNo , * from AtKosu where Atkodu = 19662 order by KosuTarihi desc;
+
+
+delete from gm_KosuTahminNominal;
+
+
+select * from gm_KosuTahminNominal;
+
+select from AtKosu where KosuKodu in (
+    select B.kosukodu from
+    (select kosukodu,count(*) as atcount from AtKosu group by kosukodu) A,
+    (select kosukodu,count(*) as atcount from AtKosu where son7derecefarkiortalamasi is not null group by kosukodu ) B
+    where A.kosukodu = B.kosukodu
+    AND A.atcount = B.atcount)
+    AND KOSUTARIHI<='2011-08-01'
+    AND KOSUTARIHI>='2011-05-01'
+    AND BirinciIleDereceFarki is not null
+      order by KosuKodu asc,SonucNo asc
+
+
+      select sonucno,* from gm_KosuTahminNominal a , atkosu  b
+      where  a.gm_kosuId = b.KosuKodu and a.gm_AtId = b.AtKodu
+      and gm_Experiment = 'test-predictions'
+      order by gm_kosuId, gm_yesprediction desc;
+
+      select * from gm_KosuTahminNominal where gm_Experiment = 'test-predictions'
+
+      select AtKodu from AtKosu where KosuKodu = 49283;
+
+
+select * from AtKosu where bitirisOrtalamasi  is not null;
+
+select * from KosuSonucuAtlar where AtKodu = 74246;
+
+      select * from Bahisler;
+
+
+      select * from changelog;
+
+      select gm_KosuId, gm_AtId, gm_Prediction,gm_YesPrediction,gm_NoPrediction ,  gm_RegressionActual, gm_RegressionPredicted, gm_RegressionError,
+b.SONUCNO  as Kacinci, b.Ganyan as Ganyan,
+Ikili, SiraliIkili
+from gm_KosuTahminNominal a,
+AtKosu b,
+(select ATKOSU.KOSUKODU as KOSUKODU, ATKODU, Tutar as Ikili
+from ATKOSU
+LEFT OUTER JOIN Bahisler
+ON  ATKOSU.KOSUKODU = Bahisler.KOSUKODU
+and Bahisler.BahisTipAdi='İKİLİ' ) as c,
+(select ATKOSU.KOSUKODU as KOSUKODU, ATKODU, Tutar as SiraliIkili
+from ATKOSU
+LEFT OUTER JOIN Bahisler
+ON  ATKOSU.KOSUKODU = Bahisler.KOSUKODU
+and Bahisler.BahisTipAdi='SIRALI İKİLİ' ) as  d
+ where a.gm_KosuId = b.KOSUKODU
+ and a.gm_AtId = b.ATKODU
+ and a.gm_KosuId = c.KOSUKODU
+ and a.gm_AtId = c.ATKODU
+ and a.gm_KosuId = d.KOSUKODU
+ and a.gm_AtId = d.ATKODU
+ and a.gm_Experiment = 'test-predictions'
+ order by gm_kosuId asc, b.SONUCNO asc
+
+ select * from changelog
+
+ create table RegressionPrediction (
+ experiment varchar(200) NOT NULL,
+ instanceId varchar(200) NOT NULL,
+ actual real NOT NULL ,
+ predicted real NOT NULL,
+ error  real NOT NULL,
+ PRIMARY KEY  ( experiment, instanceId ) );
+
+ drop table RegressionPrediction;
+
+
+ create table ClassificationPrediction (
+ experiment varchar(200) NOT NULL,
+ instanceId varchar(200) NOT NULL,
+ actual varchar(200) NOT NULL,
+ actualIndex integer NOT NULL,
+ predicted varchar(200) NOT NULL,
+ predictedIndex integer NOT NULL,
+ error  boolean NULL,
+ PRIMARY KEY  ( experiment, instanceId ) );
+
+
+ drop table ClassificationPrediction;
+
+ drop table Distribution;
+
+
+ CREATE INDEX Distribution_classificationClass on Distribution(classificationClass)
+
+  create table Distribution (
+ experiment varchar(200) NOT NULL,
+ instanceId varchar(200) NOT NULL,
+ classificationClass varchar(200) NOT NULL,
+ distribution real NOT NULL,
+ PRIMARY KEY  ( experiment, instanceId,classificationClass ) );
+
+ drop table distribution;
+
+ select * from ATKosu;
+
+ select * from ClassificationPrediction   ;
+
+  alter table atkosu add column  KosuKosuAtKodu varchar(200)
+
+  CREATE INDEX AtKosu_KosuKoduAtKodu on atkosu(KosuKoduAtKodu)
+
+  alter table atkosu add column  KosuKoduAtKodu varchar(200) ;
+
+  DROP INDEX AtKosu_KosuKoduAtKodu;
+
+ alter table AtKosu drop column KosukoduAtKodu;
+
+ select * from AtKosu;
+
+
+update AtKosu set KosuKoduAtKodu = KosuKodu || '_' || AtKodu;
+
+select * from AtKosu where kosukodu = 18527;
+
+
+  select ganyan, a.* from RegressionPrediction a, AtKosu b
+  where experiment = 'KStar'
+  and a.instanceId = b.KosuKoduAtKodu
+  order by b.KosuKodu desc, predicted asc;
+
+
+  select * from RegressionPrediction a  where experiment = 'KStar'
+  order by substring(instanceid from 1 for position('_' in instanceid) ) desc, predicted asc;
+
+
+  select a.experiment , '800'|| b.KosuKodu || '_' || '800' || b.AtKodu instanceId,actual,predicted,error from RegressionPrediction a, AtKosu b
+  where experiment = 'KStar'
+  and a.instanceId = b.KosuKoduAtKodu
+  order by b.KosuKodu desc, predicted asc;
+
+
+
+  select
+  '2001-03-28' kosutarihi,
+  hipodromkodu, hipodromyeri, cim, kum,
+  '800' || kosukodu kosukodu,
+  kosuno, onemlikosuadi, kosucinsdetayadi, grupadi, ikramiye1, ikramiye2, ikramiye3, ikramiye4, mesafe, pistuzunadi,
+  '800' || atkodu atkodu,
+  atno, atadi, sonkilo, jokeykisaadi, sahipkisaadi, startno, puan, puankum, annebabakodu, annebabakodu1, antrenorkodu, sahipkodu, jokeykodu, sonucno, derece, ganyan, atikramiye, birincilikyuzdesi, ikincilikyuzdesi, ucunculukyuzdesi, ikinciyegirisyuzdesi, ucuncuyegirisyuzdesi, bitirisortalamasi, son3derecefarkiortalamasi, son7derecefarkiortalamasi, birinciilederecefarki, son7birincilikyuzdesi, son7ikincilikyuzdesi, son7ucunculukyuzdesi, son7bitirisortalamasi,
+  '800'|| b.KosuKodu || '_' || '800' || b.AtKodu kosukoduatkodu
+   from RegressionPrediction a, AtKosu b
+  where experiment = 'KStar'
+  and a.instanceId = b.KosuKoduAtKodu
+  order by b.KosuKodu desc, predicted asc;
+
+  select * from information_schema.columns ;
+
+select concat(column_name) || ',' from
+  (select column_name from information_schema.columns where
+table_name='atkosu') a;
+
+select  * FROM ATKOSU where kosutarihi = '2001-03-28' ORDER BY KOSUTARIHI ASC;
+
+
+
+ select distinct kosutarihi from RegressionPrediction a, AtKosu b
+  where experiment = 'test-son7kosu-kstar-experiment'
+  and a.instanceId = b.KosuKoduAtKodu
+
+  order by b.KosuKodu desc, predicted asc;
+
+
+delete from AtKosu where kosutarihi = '2011-03-27' and substring (id || '' from 1 for 3) = '800'
+
+select count(*) from   AtKosu where kosutarihi = '2001-03-28'
+
+select count(*) from   AtKosu where experiment = 'test-son7kosu-kstar-experiment'  ;
+
+select * from AtKosu order by KosuTarihi asc;
+
+delete from AtKosu where KosuTarihi < '2001-04-01'
+
+  select sum(genyen) from (
+  select ganyan genyen , a.*,b.* from RegressionPrediction a, AtKosu b
+  where experiment = 'test-son7kosu-kstar-experiment'
+  and a.instanceId = b.KosuKoduAtKodu
+  and predicted < 2.1
+and actual =1
+  order by b.KosuKodu desc, predicted asc
+   ) a
+
+ select ganyan, a.*,b.* from RegressionPrediction a, AtKosu b
+  where experiment = 'KStar'
+  and a.instanceId = b.KosuKoduAtKodu
+  order by b.KosuKodu desc, predicted asc;
+
+  drop index AtKosu_KosuKoduAtKodu;
+
+  CREATE UNIQUE INDEX AtKosu_KosuKoduAtKodu on atkosu(KosuKoduAtKodu);
+
+  select version();
+  
